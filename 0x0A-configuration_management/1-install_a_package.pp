cb7a0c323dd33e058341c0e9f6dade8958b0d529
root <zentrictech@gmail.com>
2024-06-29 07:56:32 -0700

    /0x0A-configuration_managemen

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install flask v: 2.1.0 from pip3
-
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
